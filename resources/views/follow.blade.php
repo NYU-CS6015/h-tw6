efb681b398f7844b7e823669632bcd09fbdef002
Parul Joshi <paruljoshi2021@gmail.com>
2016-04-17 02:47:49 +0000

    Fixing form

--- resources/views/follow.blade.php
+++ resources/views/follow.blade.php
@@ -2,9 +2,10 @@
    <body>
 
    			Follow your friends :
-   			<form action="/follow" method="post">
+   			
 	    	@foreach ($follow as $user)
 	    	@if ($user->id != Auth::user()->id)
+	    	<form action="/follow" method="post">
 	        <p> User id: {{ $user->id}} <br/>
 	       	User name: {{ $user->name}} </p>
 		    <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -20,9 +21,11 @@
 	    	<br/>
 
 	    	@if($followersDetail)
+	    	<form action="/follow" method="post">
 		    	@foreach ($followersDetail as $follower)
 		        <p> Follower id: {{ $follower->id}} <br/>
 		       	Follower name: {{ $follower->name}} </p>
+		       	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 			    <input type="hidden" name="unfollowId" value = "{{$follower->id}}"></input>
 			    <input type="submit" value="Unfollow" name ="unfollow"></input>
 			   </form>
