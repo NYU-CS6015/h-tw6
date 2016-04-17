7af8467df6ef068ee838843f0ac2f65e23c7384f
Parul Joshi <paruljoshi2021@gmail.com>
2016-04-17 02:05:39 +0000

    Unfollow Users fixing post method-added input

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
 use DB;
-use App\Http\Controllers\Input;
+use Input;
 
 class FollowerController extends Controller
 {
